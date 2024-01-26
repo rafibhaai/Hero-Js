c662af0554ee84884eac4d0525c5a46b6babc101
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-01-26 16:53:29 +0600

    Add Files

---
+++ first.js
@@ -0,0 +1,10 @@
+// var even = parseFloat("6.2") ;
+// var odd = 7;
+// console.log(even+odd)
+// var first= (0);
+// var second =(50);
+// var total = (second/first);
+// console.log(total)
+var orrange =50;
+var lebu = 60;
+console.log(orrange*lebu)
---
+++ practice-task.js
@@ -0,0 +1,4 @@
+var myTaka = 1000;
+var billHoise = 700;
+var ferotPamu = (myTaka-billHoise)
+console.log(myTaka-billHoise)
