291c5a36ea87051b93480013d844b40f7ccacd0e
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-01-27 01:22:44 +0600

    add files

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("8")
+
+// lebu = 50;
+// apple = 60;
+// lebu=80;
+// lebu+=20
+// console.log(apple+lebu)
+
+// console.log(58/2)
+
+// if (3<6){
+//     console.log("eda ekta sotti kotha")
+// }
+// const height = 5;
+// const bcS = true;
+// if (height>4 && bcS==true){
+//     console.log("Biye Boshan Sir")
+
+// }
+// else{
+//     console.log("Biye Cancel sir")
+// }
+
+// const money =0;
+// if (money>300){
+//     console.log("mama you are rich")
+// }
+// else{
+//     if(money>100){
+//         console.log("tui gorib o na borolok o na")
+//     }
+//     else{
+//         if(money>0){
+
+//             console.log("kichu toh ache at least")
+//         }
+//         else{
+//             console.log("Shala Fokira")
+//         }
+//     }
+// }
+
+// Ternary
+const age=12;
+// if (age>18){
+//     console.log("Biye kora jabe")
+// }
+// else{
+//     console.log("Boyos Hoy nai tor")
+// }
+
+age>=18 ? console.log("Biye kora jabe") : console.log("boyoshoynai tor")
---
+++ practice-task2.js
@@ -0,0 +1,102 @@
+// Task-1
+// burgerPrice = 600;
+// cokePrice=30;
+// if(burgerPrice>500){
+//     console.log(burgerPrice)
+// }
+// else{
+//     console.log(burgerPrice+cokePrice)
+// }
+// 
+// Task2
+// weight = 50;
+// height = 10;
+// bmi = weight/((10**2));
+// bmi=26;
+// if (bmi<18.5){
+//     console.log("you are underweight")
+// }
+// else if (bmi>= 18.5 && bmi<=24.9){
+//     console.log("you are normal")
+// }
+// else if (bmi>=25 && bmi<=29.9){
+//     console.log("you are overweight")
+// }
+// else{
+//     console.log("you are obese")
+// }
+// // Task-3
+// score=30;
+// if (score >= 90 && score<=100){
+//     console.log("Kam Sharche mama A")
+// }
+// else if (score>=80 && score <=89){
+//     console.log("Kam Sharche mama B")
+// }
+// else if (score>=70 && score <=79){
+//     console.log("Kam Sharche mama C")
+// }
+// else if (score>=60 && score <=69){
+//     console.log("Kam Sharche mama C")
+// }
+// else if (score>=0 && score<=59){
+//     console.log("Sudche mama Fail")
+// }
+// else{
+//     console.log("Invalid Number")
+// }
+// // Task-4
+// nizerMarks=90;
+// friendERmarks=60;
+
+// if (nizerMarks>80){
+//     if (friendERmarks>80){
+//         console.log("Pizza Khawa")
+//     }
+//     else if ((friendERmarks<80) ||(friendERmarks>=60)){
+//         console.log("Pera nai mama next time")
+//     }
+//     else if (friendERmarks<60 || friendERmarks>=40 ){
+//         console.log("Dhurro Hala ki korli")
+//     }
+//     else if (friendERmarks<400){
+//         console.log("Halay korlo da ki")
+//     }
+// }
+// else{
+//     console.log("Hawa Hoye Gelam")
+// }
+// Task-5
+// result = 0;
+// numOne= 7;
+// numTwo= 6;
+// if (numOne>numTwo){
+//     result = (numOne*2)
+// }
+// else{
+//     result=(numOne+numTwo)
+// }
+// console.log(result)
+// Task-6
+ticket = 800;
+age=20;
+// student = true;
+
+if (age<10){
+    console.log("You are free to travel Sir")
+}
+
+
+if (age>10 && age<=60){
+    if (student==true){
+        ticket = 800 * (50/100)
+    }
+    else{
+        ticket=800
+    }
+}
+
+else{
+    ticket=800*(15/100)
+}
+console.log(ticket)
