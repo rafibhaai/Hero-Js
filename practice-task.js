c2b29fd4268d99593246e1c0a0d44faffc8bfe3d
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-01-26 17:12:46 +0600

    add Files

--- practice-task.js
+++ practice-task.js
@@ -1,4 +1,23 @@
+// Task-1
 var myTaka = 1000;
 var billHoise = 700;
 var ferotPamu = (myTaka-billHoise)
 console.log(myTaka-billHoise)
+
+// Task-2
+var mat=75.25;
+var bio= 65;
+var che= 80;
+var phy=35.45;
+var ban = 99.50;
+var avarage = ((mat+bio+che+phy+ban)/5);
+console.log(avarage. toFixed(2))
+
+
+// Task-3
+var given= 119
+console.log(given%5)
+
+// Task-4
+var a =isNan("11");
+var a =isNaN(2-10);
