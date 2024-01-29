3634f44f8978a6a927f2da0fb038f2294df198c9
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-01-30 00:04:08 +0600

    done

---
+++ practice-array.js
@@ -0,0 +1,47 @@
+// // Task-1
+// let fruits = ["apple","banana","mango","orrange","lebu",60]
+// console.log(fruits [3])
+// fruits[1]="Jambura"
+// console.log(fruits)
+
+// Task-2
+// let destiny = ["Bandorban", "Cox", "Kaptai"]
+// console.log(destiny)
+// destiny.push("Sajek")
+// destiny.push("Nikli Hawor","Kewkradong")
+// console.log(destiny)
+// destiny.pop()
+// console.log(destiny)
+
+// Task-3
+// const books = ["Bangla","English","Math","Bio","Chemistry"]
+// let hasbooks = books.includes("English")
+// if (hasbooks){
+//     console.log("Yes We find the book")
+// }
+// else{
+//     console.log("Book is not in array")
+// }
+
+// Task--4
+// let variable1 = [1, 2, 3];
+// let variable2 = "Hello, world!";
+// let variable3 = { name: "John", age: 30 };
+// let variable4 = 42;
+// let variable5 = ["apple", "banana", "orange"];
+// console.log(Array.isArray(variable1))
+// console.log(Array.isArray(variable2))
+// console.log(Array.isArray(variable3))
+// console.log(Array.isArray(variable4))
+// console.log(Array.isArray(variable5))
+
+// Task-5
+// let first = [1, 2, 3];
+// let second = ["Bandorban", "Cox", "Kaptai"];
+// let final = first.concat(second);
+// console.log("Combined Array:",final)
+
+
+
+
+
