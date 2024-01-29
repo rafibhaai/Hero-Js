fc298394be42364ca407accdfe1e7d033f655b36
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-01-30 00:54:44 +0600

    Done

---
+++ practice-loops.js
@@ -0,0 +1,40 @@
+// For Loops
+
+// Task-1
+// for (let i=1 ; i<=10; i+=1)
+// console.log("I will invest at least 6 hrs every single day for next 60 days!")
+
+// Task-2
+// for (let i=61; i<=100; i+=1){
+//     if (i%2!=0){
+//         console.log(i)
+//     }
+// }
+
+// for (let i=78; i<=98; i+=1)
+// if (i%2==0){
+//     console.log(i)
+// }
+
+// Task-3
+// let sum=0;
+// for (let i=91; i<=129; i+=1)
+// if (i%2!=0){
+//     sum+=i
+// }
+// console.log(sum)   
+
+// let sum2=0;
+// for (let i=51; i<=85; i+=1)
+// if (i%2==0){
+//     sum+=i
+// }
+// console.log(sum)   
+
+// Task-4
+// let suM=9
+// for (let i= 1; i<=10; i+=1)
+// console.log(suM*i)
+
+
+
