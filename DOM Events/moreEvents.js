11dd5c0b67a5687ad5413f0aa4828a66a73d6f80
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-02-12 02:09:50 +0600

    Add All the Files

---
+++ DOM Events/moreEvents.js
@@ -0,0 +1,10 @@
+document.getElementById("btn-more-id").addEventListener("mousemove",function (){
+    console.log("Event Triggered")
+})
+
+
+
+
+document.getElementById("text-field").addEventListener("focus",function (){
+    console.log("Joy Bangla")
+})
