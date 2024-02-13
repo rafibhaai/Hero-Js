ce1bfa07c795f25333cef80b02c0017e2f107acc
Niaz Morshed <niazmorshedrafi@gmail.com>
2024-02-13 17:04:03 +0600

    Add Files

--- Geometry Project/rectangle-area.js
+++ Geometry Project/rectangle-area.js
@@ -1,3 +1,18 @@
 function calculateRectangleArea(){
-    
+    const rectangleWidth = document.getElementById("rectangle-width");
+    const rectangleWidthText = rectangleWidth.value;
+    const width = parseFloat(rectangleWidthText)
+
+
+
+    const rectangleLength = document.getElementById("rectangle-length");
+    const rectangleLengthText = rectangleLength.value;
+    const length = parseFloat(rectangleLengthText)
+
+    const area = width * length;
+    console.log("Area Of The Rectangle is :",area)
+
+    // display Rectangle area
+    const displayRectangle = document.getElementById("rectangle-area")
+    displayRectangle.innerText = area;
 }
